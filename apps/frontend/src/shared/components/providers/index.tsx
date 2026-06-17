b43b6d6b9db5b8478b565f89bf7ba406f7ddb98a
you <you@example.com>
2026-06-17 23:42:10 +0330

    first commit

---
+++ apps/frontend/src/shared/components/providers/index.tsx
@@ -0,0 +1,18 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { TooltipProvider } from "../ui/tooltip";
+import { Toaster } from "../ui/sonner";
+
+function Providers({ children }: { children: ReactNode }) {
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { refetchOnWindowFocus: false } },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>{children}</TooltipProvider>
+      <Toaster position="bottom-center" />
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
